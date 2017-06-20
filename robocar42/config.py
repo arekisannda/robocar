7acc4c97581201d98089a40681433a709b7fba7d
Robotics <robotics@sierra-test.local>
2017-06-19 17:38:13 -0700

    Splits config to have different config for different classes

--- robocar42/config.py
+++ robocar42/config.py
@@ -28,12 +28,10 @@ if not os.path.exists(model_path):
 """
 	Loads configuration file for the program
 """
-def parser_config(config_name):
+def vehicle_parser_config(config_name):
 	config_file = os.path.join(config_path,config_name)
 	config.read(config_file)
-
 	cfg = {}
-
 	vehicle = config['vehicle']
 	cfg['car_url'] = vehicle.get('url')
 	cfg['straight'] = vehicle.get('straight')
@@ -41,7 +39,12 @@ def parser_config(config_name):
 	cfg['right'] = vehicle.get('max_right')
 	cfg['exp'] = vehicle.get('exp')
 	cfg['speed'] = vehicle.get('speed')
+	return cfg
 
+def camera_parser_config(config_name):
+	config_file = os.path.join(config_path, config_name)
+	config.read(config_file)
+	cfg = {}
 	camera = config['camera']
 	tmp = map(str,camera.get('url_list').split(','))
 	tmp = [x.strip() for x in tmp]
@@ -50,7 +53,12 @@ def parser_config(config_name):
 	cfg['cam_user'] = camera.get('user')
 	cfg['cam_pass'] = camera.get('pass')
 	cfg['cam_channel'] = camera.get('channel')
+	return cfg
 
+def display_parser_config(config_name):
+	config_file = os.path.join(config_path, config_name)
+	config.read(config_file)
+	cfg = {}
 	image = config['image']
 	cfg['oshape'] = (image.getint('oshapeX'),
 					image.getint('oshapeY'))
