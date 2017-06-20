447cb107ca3760e5a904670fd5365277614bbeb8
Alexander Chan <arekisanndachan@gmail.com>
2017-06-20 09:20:08 -0700

    Adds unit test for car.py and camera.py

---
+++ tests/cam_test.py
@@ -0,0 +1,32 @@
+import unittest
+import sys
+import os
+from os.path import dirname
+sys.path.append(dirname(dirname(os.path.realpath(__file__))))
+
+from robocar42 import config
+from robocar42.camera import Camera
+
+class TestCamera(unittest.TestCase):
+
+    def setUp(self):
+        cam_conf = config.camera_parser_config('camera_1.ini')
+        disp_conf = config.display_parser_config('display.ini')
+        self.cam = Camera("cam", cam_conf, disp_conf['oshape'], True)
+
+    def test_capture(self):
+        image = self.cam.image
+        print(image.shape)
+        print(self.cam.shape)
+        assert image.shape == self.cam.shape
+
+    def test_image(self):
+        image = self.cam.image
+        print(type(image))
+
+    def test_stop(self):
+        self.cam.stop()
+        assert self.cam.stopped == False
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/car_test.py
@@ -0,0 +1,69 @@
+import unittest
+import sys
+import os
+from os.path import dirname
+sys.path.append(dirname(dirname(os.path.realpath(__file__))))
+
+from robocar42 import config
+from robocar42.car import Car
+
+class TestCar(unittest.TestCase):
+
+    def setUp(self):
+        car_conf = config.vehicle_parser_config('vehicle.ini')
+        self.car = Car(car_conf)
+
+    def test_drive_1(self):
+        car_conf = config.vehicle_parser_config('vehicle.ini')
+        cmd = (
+            'http://' +
+            car_conf['car_url'] +
+            '/fwd' +
+            '/m' + car_conf['speed'] +
+            '/lf' + str(75)
+        )
+        print ("Car class output: %s" % self.car.drive([1, 250, -1, 75]))
+        print ("Hardcoded output: %s" % cmd)
+        assert self.car.drive([1, 250, -1, 75]) == cmd
+
+    def test_drive_2(self):
+        car_conf = config.vehicle_parser_config('vehicle.ini')
+        cmd = (
+            'http://' +
+            car_conf['car_url'] +
+            '/rev' +
+            '/m' + str(100) +
+            '/rt' + str(100)
+        )
+        print ("Car class output: %s" % self.car.drive([-1, 100, 1, 100]))
+        print ("Hardcoded output: %s" % cmd)
+        assert self.car.drive([-1, 250, 1, 100]) == cmd
+
+    def test_drive_3(self):
+        car_conf = config.vehicle_parser_config('vehicle.ini')
+        cmd = (
+            'http://' +
+            car_conf['car_url'] +
+            '/fwd' +
+            '/m' + car_conf['speed'] +
+            '/st' + car_conf['straight']
+        )
+        print ("Car class output: %s" % self.car.drive([1, 250, 0, 100]))
+        print ("Hardcoded output: %s" % cmd)
+        assert self.car.drive([1, 250, -1, 100]) == cmd
+
+    def test_drive_4(self):
+        car_conf = config.vehicle_parser_config('vehicle.ini')
+        cmd = (
+            'http://' +
+            car_conf['car_url'] +
+            '/fwd' +
+            '/m' + car_conf['speed'] +
+            '/lf' + str(100)
+        )
+        print ("Car class output: %s" % self.car.drive([1, 250, -1, 100]))
+        print ("Hardcoded output: %s" % cmd)
+        assert self.car.drive([1, 250, -1, 100]) == cmd
+
+if __name__ == "__main__":
+    unittest.main()
